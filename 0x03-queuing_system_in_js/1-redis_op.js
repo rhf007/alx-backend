530b6bd949ae3379b3f76af32a0708d0864b69b7
Rahaf Hazem <110139745+rhf007@users.noreply.github.com>
2024-12-02 06:51:58 +0200

    Create 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (error, value) => {
+    if (error) {
+      console.log(`Error retrieving value for key ${schoolName}: ${error.message}`);
+      return;
+    }
+    console.log(`${value}`);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+export { setNewSchool, displaySchoolValue };
